3a92909b129ec39b3ce64845a2c7307f084bc452
Om21o7 <122809880+Om21o7@users.noreply.github.com>
2025-03-18 22:59:23 -0400

    handled the cart actions

--- ManageX/CartManager.swift
+++ ManageX/CartManager.swift
@@ -18,9 +18,14 @@ class CartManager: ObservableObject {
             let newItem = CartItem(item: item, quantity: 1)
             cartItems.append(newItem)
         }
-        objectWillChange.send() // 🔥 Manually trigger UI update
+        objectWillChange.send() 
     }
 
+    func clearCart() {
+        cartItems.removeAll()
+        objectWillChange.send() 
+    }
+    
     func removeFromCart(item: FoodItem) {
         if let index = cartItems.firstIndex(where: { $0.item.id == item.id }) {
             if cartItems[index].quantity > 1 {
@@ -29,13 +34,10 @@ class CartManager: ObservableObject {
                 cartItems.remove(at: index)
             }
         }
-        objectWillChange.send() // 🔥 Manually trigger UI update
+        objectWillChange.send() 
     }
 
-    func clearCart() {
-        cartItems.removeAll()
-        objectWillChange.send() // 🔥 Ensure UI refreshes
-    }
+    
 
     var total: Double {
         cartItems.reduce(0) { $0 + ($1.item.price * Double($1.quantity)) }
